fa7c68b6c7c25c82a724f03ca5a2fcf870a20d97
Pharao-net <132840452+Pharao-net@users.noreply.github.com>
2024-07-12 14:40:38 +0200

    Update invoky.ps1

--- invoky.ps1
+++ invoky.ps1
@@ -1,10 +1,10 @@
 # Set the source and destination paths
-$sourcePath = "C:\Path\to\folder"
-$destinationPath = "C:\Path\to\destination"
+$sourcePath = "C:\Health"
+$destinationPath = "C:\Health2"
 
 # Copy the folder from source to destination
 Copy-Item -Path $sourcePath -Destination $destinationPath -Recurse
 
 # Start the snConnector.exe executable
-$exePath = "C:\Path\to\snConnector.exe"
+$exePath = "C:\Health\snConnector.exe"
 Start-Process -FilePath $exePath
